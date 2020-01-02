5fc4974a0d59c1cb363856cbccbe060b675a0eb4
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-02 13:58:52 -0800

    some repo json changes

---
+++ build/precache-manifest.c87dc5b6806c74139e8567befc8b2547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3780ae12d407786de6ab",
+    "url": "/karensin/karensin.github.io/static/css/main.7cd58dc7.chunk.css"
+  },
+  {
+    "revision": "3780ae12d407786de6ab",
+    "url": "/karensin/karensin.github.io/static/js/main.3780ae12.chunk.js"
+  },
+  {
+    "revision": "3a62412b7c815d0c964f",
+    "url": "/karensin/karensin.github.io/static/css/1.974388d3.chunk.css"
+  },
+  {
+    "revision": "3a62412b7c815d0c964f",
+    "url": "/karensin/karensin.github.io/static/js/1.3a62412b.chunk.js"
+  },
+  {
+    "revision": "9d52c4b5b60f5abccfa9",
+    "url": "/karensin/karensin.github.io/static/js/runtime~main.9d52c4b5.js"
+  },
+  {
+    "revision": "c2e919408b4881b29aae00af89d6db82",
+    "url": "/karensin/karensin.github.io/index.html"
+  }
+];
