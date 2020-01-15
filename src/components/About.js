9d9cdb11e4c5ff8884def2d4f96267c9fabbbec9
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-15 14:39:25 -0800

    resume and about

--- src/components/About.js
+++ src/components/About.js
@@ -29,15 +29,15 @@ class About extends Component {
                     <p className= 'aboutme'>  
                     I am a web designer/ developer /former data analyst based in San Francisco, California.
                     I am passionate in frontend development and technology. My goals are to focus on typography and create simple and user friendly UI/UX experience. 
-                    My former experience at Apple allowed me to work and improve as a team player. 
-                    I have successfully assisted and assured the precision of data gathered and assisted in implementing into Apple maps. 
-                    I have worked closely with the design team on new map features which included planning out workflows on the delivery of the features to maximize readability,
-                    interface behaviors and how information is being navigated and displayed. 
+                    My experience at Apple allowed me to work and improve as a team player. 
+                    I worked closely with designers on new map features which included planning out workflows on the delivery of the features to maximize discoverability, 
+                    interface behaviors, and how information is being navigated and displayed. I built analytics dashboards with PostgreSQL, ArcGIS, and QGIS to report on 
+                    traffic and road alterations via large datasets. 
                     </p>
                     <h3 className= 'titleName'>Values... </h3>
                     <p className='aboutme '> 
                     Growing up from a traditional Chinese family, my parents taught me to be someone that is responsible, empathic and respect others. With the morals and values they taught me, it has helped me in growing to be a great team player and a leader. 
-                    I was the president of an animal rights club and led a group 35 students in all grades in learning about animal rights, educating others and volunteering in rehabilitating strays / abandoned animals back to the society. 
+                    I was the president of an animal welfare club and led a group 35 students to learn and educate others on animal rights and welfare, rehabilitating strays / abandoned companion animals back to the society. 
                     I am also an active lead in both Solano animal shelter and San Francisco SPCA in rehabilitating companion animals. 
                     </p>
                     </section>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,9 +4,11 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render (){
       return (  
+        <div> 
      
-        <img className= 'resume' src={process.env.PUBLIC_URL + "/images/resume.jpg"}/>   
- 
+        <img className= 'resume' src={process.env.PUBLIC_URL + "/images/KarenResume1.jpg"}/>   
+        <img className= 'resume' src={process.env.PUBLIC_URL + "/images/KarenResume2.jpg"}/>   
+        </div>
       );
   }
 }
