c37d8ad5872201ba60f4bcdf89a36293d64c2f12
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-16 13:32:58 -0800

    Updates

--- precache-manifest.e74cd67843ae3f9cd7b766ba84856389.js
+++ precache-manifest.e74cd67843ae3f9cd7b766ba84856389.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "fbdbd967e832093bbf85",
+    "revision": "332ce276f78a43baf9ff",
     "url": "/portfolio2019/static/css/main.9309ca44.chunk.css"
   },
   {
-    "revision": "fbdbd967e832093bbf85",
-    "url": "/portfolio2019/static/js/main.fbdbd967.chunk.js"
+    "revision": "332ce276f78a43baf9ff",
+    "url": "/portfolio2019/static/js/main.332ce276.chunk.js"
   },
   {
-    "revision": "ee2189c934de167fda6b",
+    "revision": "07e244e2acfbe2586cef",
     "url": "/portfolio2019/static/css/1.142d571b.chunk.css"
   },
   {
-    "revision": "ee2189c934de167fda6b",
-    "url": "/portfolio2019/static/js/1.ee2189c9.chunk.js"
+    "revision": "07e244e2acfbe2586cef",
+    "url": "/portfolio2019/static/js/1.07e244e2.chunk.js"
   },
   {
     "revision": "12f7caadf4d69b60e868",
     "url": "/portfolio2019/static/js/runtime~main.12f7caad.js"
   },
   {
-    "revision": "1b2e5e00eff887bb6b9cd5fbee1e0a98",
+    "revision": "3b24b99e12b7966278f662d3331cd82d",
     "url": "/portfolio2019/index.html"
   }
 ];
