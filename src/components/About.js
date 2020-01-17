cadca060d065148a3cd6794ebf682011959e9fd9
Jason Jiang <jjiang1991@gmail.com>
2020-01-17 10:45:20 -0800

    karen sin -> karensin

--- src/components/About.js
+++ src/components/About.js
@@ -26,13 +26,13 @@ class About extends Component {
             I worked closely with designers on new map features which included planning out workflows on the delivery of the features to maximize discoverability,
             interface behaviors, and how information is being navigated and displayed. I built analytics dashboards with PostgreSQL, ArcGIS, and QGIS to report on
             traffic and road alterations via large datasets.
-                    </p>
+          </p>
           <h3 className='titleName'>Values... </h3>
           <p className='aboutme '>
             Growing up from a traditional Chinese family, my parents taught me to be someone that is responsible, empathic and respect others. With the morals and values they taught me, it has helped me in growing to be a great team player and a leader.
             I was the president of an animal welfare club and led a group 35 students to learn and educate others on animal rights and welfare, rehabilitating strays / abandoned companion animals back to the society.
             I am also an active lead in both Solano animal shelter and San Francisco SPCA in rehabilitating companion animals.
-                    </p>
+          </p>
         </section>
       </div>
     );
