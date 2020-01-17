125bd686df6ac39298dab80e6414266904acda39
Jason Jiang <jjiang1991@gmail.com>
2020-01-17 00:12:53 -0800

    Updates

--- precache-manifest.2015a672bb65bcdfdce1eeefc95d6725.js
+++ precache-manifest.2015a672bb65bcdfdce1eeefc95d6725.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ec1d277ff855a9d99e37",
-    "url": "/portfolio2019/static/css/main.0d13db95.chunk.css"
+    "revision": "397e39ea2ae93a88b56d",
+    "url": "/portfolio2019/static/css/main.c1cc9207.chunk.css"
   },
   {
-    "revision": "ec1d277ff855a9d99e37",
-    "url": "/portfolio2019/static/js/main.ec1d277f.chunk.js"
+    "revision": "397e39ea2ae93a88b56d",
+    "url": "/portfolio2019/static/js/main.397e39ea.chunk.js"
   },
   {
     "revision": "2c49e142dd30ce0dfde5",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio2019/static/js/runtime~main.12f7caad.js"
   },
   {
-    "revision": "3bc1d3bdac74356c2176692cabc85b0f",
+    "revision": "a317599af4f3014221599b6a6c5561c7",
     "url": "/portfolio2019/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio2019/precache-manifest.e27ba27a0fa2c2c6bbce4e26949465d5.js"
+  "/portfolio2019/precache-manifest.2015a672bb65bcdfdce1eeefc95d6725.js"
 );
 
 workbox.clientsClaim();
