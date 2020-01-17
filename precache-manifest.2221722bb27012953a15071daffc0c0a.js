28bfefc232fd89a57f0d738e76007411d711538e
Jason Jiang <jjiang1991@gmail.com>
2020-01-16 22:16:40 -0800

    Updates

---
+++ precache-manifest.2221722bb27012953a15071daffc0c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6e99f68b48eb8f39137",
+    "url": "/portfolio2019/static/css/main.f0703fae.chunk.css"
+  },
+  {
+    "revision": "b6e99f68b48eb8f39137",
+    "url": "/portfolio2019/static/js/main.b6e99f68.chunk.js"
+  },
+  {
+    "revision": "ff59d1dec3e935678fd9",
+    "url": "/portfolio2019/static/css/1.05f387ef.chunk.css"
+  },
+  {
+    "revision": "ff59d1dec3e935678fd9",
+    "url": "/portfolio2019/static/js/1.ff59d1de.chunk.js"
+  },
+  {
+    "revision": "12f7caadf4d69b60e868",
+    "url": "/portfolio2019/static/js/runtime~main.12f7caad.js"
+  },
+  {
+    "revision": "78e529bef0ca99290d3056fbf2db2f64",
+    "url": "/portfolio2019/index.html"
+  }
+];
