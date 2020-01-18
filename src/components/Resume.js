2c052214a4de38b9200363166530b81bea871d03
Karen k  sin <karenksin@MacBook-Pro.local>
2020-01-17 17:41:08 -0800

    whelpo

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf'
 
 
 class Resume extends Component {
+  componentDidMount() {
+    pdfjs.GlobalWorkerOptions.workerSrc = 'cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js';
+    }
+
+    
   render() {
+    
     return (
       <div className='resume' id='resume'>
         <article>
+          
+          <Document file={process.env.PUBLIC_URL + "/images/KarenResume2019.pdf"} > 
+            <Page  pageNumber= '1' />
+          </Document>
           <img alt='resume page 1' className='resumePage' src={process.env.PUBLIC_URL + "/images/KarenResume1.jpg"} />
           <img alt='resume page 2' className='resumePage' src={process.env.PUBLIC_URL + "/images/KarenResume2.jpg"} />
         </article>
