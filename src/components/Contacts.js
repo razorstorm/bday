7e2e58259ac9c6e74cfd94968a012dd93366daf3
Jason Jiang <jjiang1991@gmail.com>
2020-01-18 15:05:31 -0800

    header fonts are thin

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -10,15 +10,15 @@ class Contacts extends Component {
                 <section className='contacts' id="contacts">
                     <h3> Get in Touch </h3>
                     <section className='contactsList'>
-                        <a target="_blank" href="mailto: kkwsin@gmail.com"> 
+                        <a target="_blank" href="mailto: kkwsin@gmail.com">
                             <i class="fas fa-2x fa-envelope"></i>
                         </a>
                         <a target="_blank" href='https://www.linkedin.com/in/karensin94/'>
                             <i className="fab fa-2x fa-linkedin"></i>
-                        </a> 
+                        </a>
                         <a target="_blank" href='https://github.com/karensin'>
                             <i class="fab fa-2x fa-github"></i>
-                        </a> 
+                        </a>
                         <a target="_blank" href='https://angel.co/karen-kaweng-sin'>
                             <i class="fab fa-2x fa-angellist"></i>
                         </a>
