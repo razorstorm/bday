79be98f7060ef7cc5890db8e0e215aefcde39041
Jason Jiang <jjiang1991@gmail.com>
2020-01-18 12:25:32 -0800

    profile pic and rewrote the description a bit

--- src/components/About.js
+++ src/components/About.js
@@ -15,24 +15,29 @@ class About extends Component {
             </div>
           </div>
         </Header>
-        <section className='aboutmeSection'>
-          <h2 className="pageTitle">About Me</h2>
-          <h3 className='titleName'>Experience... </h3>
-          <p className='aboutme'>
-            I am a web designer/ developer /former data analyst based in San Francisco, California.
-            I am passionate in frontend development and technology. My goals are to focus on typography and create simple and user friendly UI/UX experience.
-            My experience at Apple allowed me to work and improve as a team player.
-            I worked closely with designers on new map features which included planning out workflows on the delivery of the features to maximize discoverability,
-            interface behaviors, and how information is being navigated and displayed. I built analytics dashboards with PostgreSQL, ArcGIS, and QGIS to report on
-            traffic and road alterations via large datasets.
+        <div className='aboutmeWrapper'>
+          <section className='aboutmeSection'>
+            <h2 className="pageTitle">About Me</h2>
+            <div className="aboutProfilePicWrapper">
+              <img className="aboutProfilePic" src={process.env.PUBLIC_URL + "/images/purty.jpeg"} />
+            </div>
+            <h3 className='titleName'>Experience... </h3>
+            <p className='aboutme'>
+              I am a web <strong>designer / developer / former data analyst</strong> based in <strong>San Francisco, California</strong>.
+              I am passionate in <strong>frontend development</strong> and technology.
+              My goals are to focus creating simple and user friendly UI/UX experiences.
+              My experience at Apple allowed me to work and improve as a team player. I worked closely with designers on new map features which included planning out workflows on the delivery of the features to maximize discoverability,
+              interface behaviors, and how information is being navigated and displayed. I built analytics dashboards with <strong>PostgreSQL, ArcGIS, and QGIS</strong> to report on
+              traffic and road alterations via large datasets.
           </p>
-          <h3 className='titleName'>Values... </h3>
-          <p className='aboutme '>
-            Growing up from a traditional Chinese family, my parents taught me to be someone that is responsible, empathic and respect others. With the morals and values they taught me, it has helped me in growing to be a great team player and a leader.
-            I was the president of an animal welfare club and led a group 35 students to learn and educate others on animal rights and welfare, rehabilitating strays / abandoned companion animals back to the society.
-            I am also an active lead in both Solano animal shelter and San Francisco SPCA in rehabilitating companion animals.
+            <h3 className='titleName'>Values... </h3>
+            <p className='aboutme '>
+              Growing up in a traditional Chinese family, my parents taught me to be responsible, empathic and respectful towards others. With the morals and values they taught me, I've grown to be a great team player and a responsible leader.
+              I was the <strong>president of an animal welfare club</strong> and led a group 35 students to learn and educate others on <strong>animal rights and welfare</strong>, rehabilitating strays / abandoned companion animals back to the society.
+              I am also an active lead in both Solano animal shelter and San Francisco SPCA in rehabilitating companion animals.
           </p>
-        </section>
+          </section>
+        </div>
       </div>
     );
   }
