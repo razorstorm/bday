64db60894a340475ab5c29d9f5d29acb0aa0b900
Karen k  sin <karenksin@MacBook-Pro.local>
2020-01-17 17:09:42 -0800

    icons

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -8,15 +8,25 @@ class Contacts extends Component {
         return (
             <div>
                 <section className='contacts' id="contacts">
-                    <h3> Get in touch </h3>
+                    <h3> Get in Touch </h3>
                     <section className='contactsList'>
-                        <a href="mailto: kkwsin@gmail.com">kkwsin@gmail.com</a>
-                        <a href='https://www.linkedin.com/in/karensin94/'>Linkedin</a> 
-                        <a href='https://github.com/karensin'>Github</a> <i style={{background: 'blue'}} className="fas fa-linkedin-in"></i>
-                        <a href='https://angel.co/karen-kaweng-sin'>Angel.co</a>
+                        <a target="_blank" href="mailto: kkwsin@gmail.com"> 
+                            <i class="fas fa-2x fa-envelope"></i>
+                        </a>
+                        <a target="_blank" href='https://www.linkedin.com/in/karensin94/'>
+                            <i className="fab fa-2x fa-linkedin"></i>
+                        </a> 
+                        <a target="_blank" href='https://github.com/karensin'>
+                            <i class="fab fa-2x fa-github"></i>
+                        </a> 
+                        <a target="_blank" href='https://angel.co/karen-kaweng-sin'>
+                            <i class="fab fa-2x fa-angellist"></i>
+                        </a>
                     </section>
                 </section>
-                <footer>Karensin 2019</footer>
+                <footer>
+                    <span className="copyright">Karen Sin 2019</span>
+                </footer>
             </div>
         );
     }
