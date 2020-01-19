36acac0d61b7664f352409afe142106df9c04001
Jason Jiang <jjiang1991@gmail.com>
2020-01-18 18:01:35 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio2019/precache-manifest.be3aeb9696cbb2643be336bee51261fd.js"
+  "/portfolio2019/precache-manifest.5af5550ca378f24d43203ef8dae3caae.js"
 );
 
 workbox.clientsClaim();
