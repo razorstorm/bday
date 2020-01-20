7347bab57d7618c200f2de0ebab5dc70c1883372
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-19 18:32:07 -0800

    nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,24 @@ import Projects from './Projects.js';
 import Header from './Header.js';
 import About from './About.js';
 import { HashLink as Link } from 'react-router-hash-link';
+import Contacts from './Contacts';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <Header className="home-header intro-header">
-          <div className="title">
-            <h1> KAREN SIN </h1>
-            <h4> Developer | Web Designer | Data Analyst </h4>
-            <Link to="/#contacts" className="btn collab-btn">Let's Collab</Link>
+          <div className="container h-100">
+            <div className="row h-100 align-items-center">
+              <div className="col">
+                <div className="title">
+                  <h1> KAREN SIN </h1>
+                  <h4> Developer | Web Designer | Data Analyst </h4>
+
+                  <Link to="/#contacts" className="btn collab-btn">Let's Collab</Link>
+                </div>
+              </div>
+            </div>
           </div>
           <section className="scrolldown"><Link to="/#projects"><i className="fa fa-angle-double-down"></i></Link></section>
         </Header>
